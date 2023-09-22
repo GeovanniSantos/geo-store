2d9a905176c77ae6608eb5cdae7d073c972983e8
Your Name <gfc.net@gmail.com>
2023-09-22 16:11:36 -0300

    Backend create

---
+++ backend/src/main/java/com/ecommerce/geostore/GeostoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.geostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeostoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ecommerce/geostore/GeostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.geostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
